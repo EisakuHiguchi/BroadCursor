317a95967e5fed3204a19c4ea825d4db65b4b2e8
EisakuHiguchi <13ec083@ms.dendai.ac.jp>
2016-02-01 10:24:17 +0900

    広域カーソル

---
+++ broadcursor/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace broadcursor
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BroadCursor bc = new BroadCursor(this);
+            // カーソルで動かしたいコントロールを渡すだけ
+            bc.addControl(monthCalendar1);
+            bc.addControl(listBox1);
+            bc.addControl(textBox1);
+            bc.addControl(button1);
+            
+        }
+    }
+}
